b902acd38c19d8c8590dbfdc42d06c37fe5ed281
Ivaylo <ignatov.ivaylo@gmail.com>
2022-10-31 11:06:05 +0200

    querystring module

--- intro-to-node-js/index.js
+++ intro-to-node-js/index.js
@@ -1,7 +1,10 @@
 const url = require('url');
-
-const baseUrl = 'https://softuni.bg/about';
+const querystring = require('querystring');
+const baseUrl = 'https://softuni.bg/about/code/?year=2017&month=february';
 
 const parseUrl = url.parse(baseUrl);
 
 console.log(parseUrl)
+
+const qs = querystring.parse(parseUrl.query);
+console.log(qs)
