90835c30e2fc2e2d0bda3efe4b1a2aed238f158d
Ivaylo <ignatov.ivaylo@gmail.com>
2022-10-31 11:27:27 +0200

    different paths different messages

--- intro-to-node-js/server.js
+++ intro-to-node-js/server.js
@@ -1,6 +1,16 @@
 const http = require('http');
+const url = require('url');
 
 http.createServer((req, res) => {
-    res.write('Hello World');
-    res.end();
+    const urlBase = url.parse(req.url)
+    const path = urlBase.pathname;
+    if (path === '/') {
+        res.write('Hello World');
+        res.end();
+    } else if (path === '/cat') {
+        res.write('Hello Cats');
+        res.end();
+    }
+
+
 }).listen(5000, () => console.log('Server is running on port 5000...'))
