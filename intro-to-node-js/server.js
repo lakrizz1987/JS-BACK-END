2426f8eeb25e5256509b76084b3b83d4317edf59
Ivaylo <ignatov.ivaylo@gmail.com>
2022-10-31 11:41:19 +0200

    writing headers

--- intro-to-node-js/server.js
+++ intro-to-node-js/server.js
@@ -1,15 +1,33 @@
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
 
 http.createServer((req, res) => {
     const urlBase = url.parse(req.url)
     const path = urlBase.pathname;
-    if (path === '/') {
-        res.write('Hello World');
-        res.end();
+    if (path === '/' && req.method === 'GET') {
+        fs.readFile('./HelloWorld.html', (err, data) => {
+            if(err){
+                alert('Some Error');
+                return
+            }
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            })
+            res.write(data);
+            res.end();
+        })
+       
     } else if (path === '/cat') {
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        })
         res.write('Hello Cats');
         res.end();
+    }else{
+        res.writeHead(404)
+        res.write(`<h1>No Data!</h1>`)
+        res.end()
     }
 
 
