0794efe9f0daf9255c0670ba5cb1cb840ea5a73d
Ivaylo <ignatov.ivaylo@gmail.com>
2022-11-02 09:46:37 +0200

    pipe stream

--- node-streams-and-utilities/streams.js
+++ node-streams-and-utilities/streams.js
@@ -1,17 +1,20 @@
 const fs = require('fs');
 
-const readStream = fs.createReadStream('./HelloWorld.html', {encoding:'utf-8'});
-const writeStream = fs.createWriteStream('./test.txt')
+const readStream = fs.createReadStream('./HelloWorld.html', { encoding: 'utf-8' });
+const writeStream = fs.createWriteStream('./pipe.html')
 
-readStream.on('data', (data)=>{
-    console.log(data)
-})
+//readStream.on('data', (data) => {
+//    console.log(data)
+//    writeStream.write(data)
+//})
 
-readStream.on('end',()=>{
-    console.log('Reading End!')
-})
+//readStream.on('end', () => {
+//    console.log('Reading End!')
+//})
 
-writeStream.write('Hello form Stream!')
+//writeStream.write('Hello form Stream!')
+
+readStream.pipe(writeStream)
 
 
 
