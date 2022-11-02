7423e9ef65e50a7573ca4a7efbdf7cf09d40ba39
Ivaylo <ignatov.ivaylo@gmail.com>
2022-11-02 15:28:35 +0200

    fs module

---
+++ node-streams-and-utilities/fs.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+/* fs.mkdir('./myDir',(err)=>{
+if(err){
+        return console.error(err.message)
+    }
+}) */
+
+fs.readdir('.' , (err,data)=>{
+    if(err){
+        return console.error(err.message)
+    }
+
+    console.log(data)
+})
+
+//fs.renameSync('./test.txt' , './test1.txt')
+
+
+fs.writeFileSync('./test1.txt', 'Ivo is studing!')
