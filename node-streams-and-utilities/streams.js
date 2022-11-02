50c55ee1cf4c791f0ed40a07fb8f6eb77ea54f40
Ivaylo <ignatov.ivaylo@gmail.com>
2022-11-02 09:41:46 +0200

    write stream

--- node-streams-and-utilities/streams.js
+++ node-streams-and-utilities/streams.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 const readStream = fs.createReadStream('./HelloWorld.html', {encoding:'utf-8'});
+const writeStream = fs.createWriteStream('./test.txt')
 
 readStream.on('data', (data)=>{
     console.log(data)
@@ -9,3 +10,8 @@ readStream.on('data', (data)=>{
 readStream.on('end',()=>{
     console.log('Reading End!')
 })
+
+writeStream.write('Hello form Stream!')
+
+
+
