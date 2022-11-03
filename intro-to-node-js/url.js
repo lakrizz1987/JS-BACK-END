3dee35b0ff2e69c3c458c378664519e5a3fc1490
Ivaylo <ignatov.ivaylo@gmail.com>
2022-11-03 12:57:26 +0200

    test

--- intro-to-node-js/url.js
+++ intro-to-node-js/url.js
@@ -4,5 +4,16 @@ const url = require('url');
 const queryString = require('querystring');
 
 //console.log(url.parse(urlBase));
+//console.log(queryString.parse(url.parse(urlBase).search));
+
+//console.log(queryString.parse(url.parse(urlBase).query));
+
+//console.log(url.format(urlBase))
+
+const urlObj = new URL(urlBase);
+console.log(urlObj.searchParams.values())
+   
+
+//console.log(urlObj.searchParams);
 
 
