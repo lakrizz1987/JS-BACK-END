836bc340a5f130a109d18d06414a0a1a52cc921a
Ivaylo <ignatov.ivaylo@gmail.com>
2022-11-04 09:34:51 +0200

    status

--- express-and-templating/demo.js
+++ express-and-templating/demo.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello from Express!')
+    res.status(200);
+    res.send('<h1>Hello from Express!</h1>');
 })
 
-app.listen(5000, ()=> console.log('Server is running on port 5000...'))
+app.listen(5000, () => console.log('Server is running on port 5000...'))
