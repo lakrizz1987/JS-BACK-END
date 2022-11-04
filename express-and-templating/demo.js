6b84cb84591b00b6caa2ded3586dbe0a573cbdfc
Ivaylo <ignatov.ivaylo@gmail.com>
2022-11-04 11:13:07 +0200

    res.sendFile

--- express-and-templating/demo.js
+++ express-and-templating/demo.js
@@ -28,9 +28,13 @@ app.route('/chain')
     })
 
 
-app.get('/download', (req, res)=>{
+app.get('/download', (req, res) => {
     res.download('./logo-express.png');
     //res.attachment('./logo-express.png', {root: __dirname});
+})
+
+app.get('/view', (req, res) => {
+    res.sendFile('./form.html', {root: __dirname})
 })
 
 app.listen(5000, () => console.log('Server is running on port 5000...'))
