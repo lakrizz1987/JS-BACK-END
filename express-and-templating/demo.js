db5d835c0a77f4039185525508e3a69c656b283c
Ivaylo <ignatov.ivaylo@gmail.com>
2022-11-04 09:39:13 +0200

    post req

--- express-and-templating/demo.js
+++ express-and-templating/demo.js
@@ -7,4 +7,9 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello from Express!</h1>');
 })
 
+app.post('/create', (req, res)=>{
+    res.status(201);
+    res.send('<h1>Data created!</h1>')
+})
+
 app.listen(5000, () => console.log('Server is running on port 5000...'))
