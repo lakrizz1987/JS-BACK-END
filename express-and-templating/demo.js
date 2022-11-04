7d117c5e2aa4e9ed8d99d1b23aa834fe118f3ee3
Ivaylo <ignatov.ivaylo@gmail.com>
2022-11-04 09:44:01 +0200

    using params

--- express-and-templating/demo.js
+++ express-and-templating/demo.js
@@ -7,9 +7,16 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello from Express!</h1>');
 })
 
-app.post('/create', (req, res)=>{
+app.post('/create', (req, res) => {
     res.status(201);
     res.send('<h1>Data created!</h1>')
+})
+
+app.get('/user/:name/:family', (req, res) => {
+    res.status(200);
+    const name = req.params.name;
+    const family = req.params.family;
+    res.send(`<h1>Hello ${name.toUpperCase()} ${family.toUpperCase()}</h1>`)
 })
 
 app.listen(5000, () => console.log('Server is running on port 5000...'))
