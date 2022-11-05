951baaf2a862b154a005387ce8748474757be696
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2022-11-05 18:27:18 +0200

    create persistance

--- express-and-templating/to-do-app/data/data.js
+++ express-and-templating/to-do-app/data/data.js
@@ -1,7 +1,16 @@
-const todos = ['Learn JS'];
+const fs = require('fs');
+const todosData = require('./collection.json');
+const todos = todosData.slice();
+console.log(todosData)
 
 const addTodo = (name) => {
     todos.push(name)
+    fs.writeFile('./data/collection.json', JSON.stringify(todos), (err) => {
+        if (err) {
+            console.log('ERROR NOW:' + err)
+        }
+        console.log('Add new data!')
+    })
 };
 
 const getAll = () => {
