0e9f53e76fd2a95011f43d3d89f25f8db76b6144
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2022-11-06 19:14:18 +0200

    Create cube.js

---
+++ workshop-cubicle/models/cube.js
@@ -0,0 +1,11 @@
+class Cube {
+    constructor(id, name, decsription, imageUrl, difficultyLevel) {
+        this.id = id;
+        this.name = name;
+        this.decsription = decsription;
+        this.imageUrl = imageUrl;
+        this.difficultyLevel = difficultyLevel;
+    }
+}
+
+module.exports = Cube;
