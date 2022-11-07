a6901bada3ae825706318f38a8f21c8961715785
Ivaylo <ignatov.ivaylo@gmail.com>
2022-11-07 16:16:37 +0200

    add new cube

--- workshop-cubicle/controler/productControler.js
+++ workshop-cubicle/controler/productControler.js
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
 
 router.get('/search', (req,res)=>{
    // const querryObj = querryString.parse(req.query)
-    console.log(req.query.search)
+    console.log(req.query)
     const filteredData = collectionManger.getOneByName(req.query.search);
     res.render('home', { products: filteredData })
 })
