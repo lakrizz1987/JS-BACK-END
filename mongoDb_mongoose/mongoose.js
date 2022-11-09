583a07344eb1ab564bbab84239655401b102f93e
Ivaylo <ignatov.ivaylo@gmail.com>
2022-11-09 13:23:45 +0200

    mongoose demo

---
+++ mongoDb_mongoose/mongoose.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const uri = 'mongodb://localhost:27017/Girls';
+
+mongoose.connect(uri);
+
+const db = mongoose.connection;
+
+db.once('open', () => {
+    console.log('Connected....')
+})
+db.on('error', () => console.log('ERRROOORRR!!!'));
+
+const mySchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    hair: String
+})
+
+const Girl = mongoose.model('Girl', mySchema);
+
+const women = new Girl({ name: 'Moni', age: 32, hair: 'long' })
+//women.save()
+
+//Girl.find({ name: 'Moni' }).then(res => console.log(res))
+
+//Girl.findOne({name:'Moni'}).then(res=>console.log(res))
+
+Girl.count().then(res=>console.log(res))
