f9e03de9b3b84b88915f3a5d0b97de4c5c4b6597
Ivaylo <ignatov.ivaylo@gmail.com>
2022-11-10 09:39:58 +0200

    rename folder

--- workshop-cubicle/config/configHandlebars.js
+++
@@ -1,8 +0,0 @@
-const handlebars = require('express-handlebars');
-
-function configHandlebars(app) {
-    app.engine('hbs', handlebars.engine())
-    app.set('view engine', 'hbs');
-}
-
-module.exports = configHandlebars;
