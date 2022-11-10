016052225d544202d6e584ccecfc4dad6283557c
Ivaylo <ignatov.ivaylo@gmail.com>
2022-11-10 12:29:41 +0200

    create model.js

---
+++ workshop-cubicle-part-2/models/model.js
@@ -0,0 +1,40 @@
+const products = require('../data/data.json');
+const fs = require('fs/promises');
+const uniqid = require('uniqid');
+
+class Model {
+    save(){
+        products.push(this)
+        return fs.writeFile(__dirname + '/../data/data.json', JSON.stringify(products))
+    }
+
+    static getAll(){
+        return products
+    }
+
+    static getOne(id){
+        const searchedProduct = products.find(x => x.id === id);
+        return searchedProduct;
+    }
+
+    static getOneBySearch(querry) {
+        let result = products;
+    
+        if (querry.search) {
+    
+            result = result.filter(x => x.name.toLowerCase().includes(querry.search.toLowerCase()));
+        };
+    
+        if (querry.from) {
+            result = result.filter(x => Number(x.difficultyLevel) >= Number(querry.from))
+        };
+    
+        if (querry.to) {
+            result = result.filter(x => Number(x.difficultyLevel) <= Number(querry.to))
+        };
+    
+        return result;
+    }
+}
+
+module.exports = Model;
