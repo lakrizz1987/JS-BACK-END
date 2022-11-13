865550683b22ef92a225ab815cfd146f1ca83635
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2022-11-13 11:18:22 +0200

    JWT intro

---
+++ sessions-and-authentication/jwt.js
@@ -0,0 +1,9 @@
+const jwt = require('jsonwebtoken');
+
+const payload = { _id: 'ivo', username: 'pass123' }
+const secret = 'MySecretKey';
+const options = { expiresIn: '2d' };
+
+const token = jwt.sign(payload,secret,options);
+
+console.log(token)
